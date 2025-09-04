e0c8e1dc983accad84bdedba8edc151a90c2e01f
bordonz <rodrigozbordon@gmail.com>
2025-09-03 21:45:42 -0300

    Metodos del directorio

--- src/tp3ejercicio5/DirectorioTelefonico.java
+++ src/tp3ejercicio5/DirectorioTelefonico.java
@@ -1,7 +1,52 @@
 package tp3ejercicio5;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 public class DirectorioTelefonico {
-    private TreeMap<Long, Contacto> directorio = new TreeMap<>();
+    private TreeMap<Long, Contacto> directorio;
+    
+    public DirectorioTelefonico(){
+        this.directorio = new TreeMap<>();
+    }
+
+    public void agregarContacto(Long nroTelefono, Contacto contacto){
+        directorio.put(nroTelefono, contacto);
+    }
+    
+    public Contacto buscarContacto(Long keyBuscada){
+        return directorio.get(keyBuscada);
+    }
+    
+    public Set<Long> buscarTelefono(String apellido){
+        Set<Long> asociados = new HashSet<>();
+        for(Map.Entry<Long, Contacto> entry : directorio.entrySet()){
+            if(entry.getValue().getApellido().equalsIgnoreCase(apellido)){
+                asociados.add(entry.getKey());
+            }
+        }
+        return asociados;
+    }
+
+    public ArrayList<Contacto> buscarContactos(String ciudad){
+        ArrayList<Contacto> listaDeContactos = new ArrayList<>();
+        for(Contacto aux : directorio.values()){
+            if(aux.getCiudad().equalsIgnoreCase(ciudad)){
+                listaDeContactos.add(aux);
+            }
+        }
+        return listaDeContactos;
+    }
+    
+    public void borrarContacto(long borrarContacto){
+        if(directorio.containsKey(borrarContacto)){
+            directorio.remove(borrarContacto);
+            System.out.println("Contacto borrado");
+        }else{
+            System.out.println("El contacto no existe");
+        }
+    }
 }

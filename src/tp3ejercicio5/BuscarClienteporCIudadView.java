51c6f65ba63ffab1205cb0e8ecc79efe896937a3
MCMathot <mcmathot@yahoo.com.ar>
2025-09-15 12:04:17 -0300

    Avances JFrame BuscarClienteCiudad
    Se completa interface gráfica en vista BuscarClientePorCiudad

--- src/tp3ejercicio5/BuscarClienteporCIudadView.java
+++ src/tp3ejercicio5/BuscarClienteporCIudadView.java
@@ -27,7 +27,7 @@ public class BuscarClienteporCIudadView extends javax.swing.JInternalFrame {
           modelo.addColumn("Direccion");
            modelo.addColumn("Ciudad");
             modelo.addColumn("Telefono");
-            jTbusquedaCiudad.setModel(modelo);
+            tablaCliente.setModel(modelo);
    }
    
    
@@ -35,66 +35,99 @@ public class BuscarClienteporCIudadView extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jcbCiudades = new javax.swing.JComboBox<>();
+        jpBuscarClientePorCiudad = new javax.swing.JPanel();
+        jlbBucarClientePorCiudad = new javax.swing.JLabel();
+        jlbCiudades = new javax.swing.JLabel();
+        cmbCiudades = new javax.swing.JComboBox<>();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTbusquedaCiudad = new javax.swing.JTable();
+        tablaCliente = new javax.swing.JTable();
+        jButton1 = new javax.swing.JButton();
 
-        jLabel1.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(0, 0, 0));
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Busqueda de Ciudad de Direccion por Ciudad");
+        jpBuscarClientePorCiudad.setBackground(java.awt.Color.lightGray);
+        jpBuscarClientePorCiudad.setForeground(java.awt.Color.black);
 
-        jLabel2.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
-        jLabel2.setText("Ciudades:");
+        jlbBucarClientePorCiudad.setBackground(java.awt.Color.lightGray);
+        jlbBucarClientePorCiudad.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jlbBucarClientePorCiudad.setForeground(new java.awt.Color(0, 0, 0));
+        jlbBucarClientePorCiudad.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jlbBucarClientePorCiudad.setText("Buscar Clientes de Directorio por Ciudad");
 
-        jcbCiudades.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jlbCiudades.setBackground(java.awt.Color.lightGray);
+        jlbCiudades.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jlbCiudades.setForeground(java.awt.Color.black);
+        jlbCiudades.setText("Ciudades:");
 
-        jTbusquedaCiudad.setModel(new javax.swing.table.DefaultTableModel(
+        tablaCliente.setBackground(java.awt.Color.white);
+        tablaCliente.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        tablaCliente.setForeground(java.awt.Color.black);
+        tablaCliente.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "DNI", "Apellido", "Nombre", "Dirección", "Ciudad", "Teléfono"
             }
         ));
-        jScrollPane1.setViewportView(jTbusquedaCiudad);
+        jScrollPane1.setViewportView(tablaCliente);
+
+        jButton1.setBackground(java.awt.Color.lightGray);
+        jButton1.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jButton1.setForeground(java.awt.Color.black);
+        jButton1.setText("Salir");
+
+        javax.swing.GroupLayout jpBuscarClientePorCiudadLayout = new javax.swing.GroupLayout(jpBuscarClientePorCiudad);
+        jpBuscarClientePorCiudad.setLayout(jpBuscarClientePorCiudadLayout);
+        jpBuscarClientePorCiudadLayout.setHorizontalGroup(
+            jpBuscarClientePorCiudadLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jpBuscarClientePorCiudadLayout.createSequentialGroup()
+                .addGroup(jpBuscarClientePorCiudadLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jpBuscarClientePorCiudadLayout.createSequentialGroup()
+                        .addGap(224, 224, 224)
+                        .addComponent(jlbBucarClientePorCiudad, javax.swing.GroupLayout.PREFERRED_SIZE, 440, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 210, Short.MAX_VALUE))
+                    .addGroup(jpBuscarClientePorCiudadLayout.createSequentialGroup()
+                        .addGap(19, 19, 19)
+                        .addGroup(jpBuscarClientePorCiudadLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jlbCiudades, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(cmbCiudades, javax.swing.GroupLayout.PREFERRED_SIZE, 172, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addComponent(jScrollPane1)))
+                .addGap(6, 6, 6))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jpBuscarClientePorCiudadLayout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(65, 65, 65))
+        );
+        jpBuscarClientePorCiudadLayout.setVerticalGroup(
+            jpBuscarClientePorCiudadLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jpBuscarClientePorCiudadLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jpBuscarClientePorCiudadLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jpBuscarClientePorCiudadLayout.createSequentialGroup()
+                        .addComponent(jlbBucarClientePorCiudad, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(38, 38, 38)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 185, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jpBuscarClientePorCiudadLayout.createSequentialGroup()
+                        .addGap(71, 71, 71)
+                        .addComponent(jlbCiudades, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(cmbCiudades, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(18, 18, 18)
+                .addComponent(jButton1)
+                .addContainerGap(19, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(43, 43, 43)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jcbCiudades, javax.swing.GroupLayout.PREFERRED_SIZE, 172, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 46, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 597, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(29, 29, 29))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(221, 221, 221)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 440, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(jpBuscarClientePorCiudad, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(42, 42, 42)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 176, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(71, 71, 71)
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jcbCiudades, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(173, 173, 173))
+            .addComponent(jpBuscarClientePorCiudad, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
@@ -102,10 +135,12 @@ public class BuscarClienteporCIudadView extends javax.swing.JInternalFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
+    private javax.swing.JComboBox<String> cmbCiudades;
+    private javax.swing.JButton jButton1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTbusquedaCiudad;
-    private javax.swing.JComboBox<String> jcbCiudades;
+    private javax.swing.JLabel jlbBucarClientePorCiudad;
+    private javax.swing.JLabel jlbCiudades;
+    private javax.swing.JPanel jpBuscarClientePorCiudad;
+    private javax.swing.JTable tablaCliente;
     // End of variables declaration//GEN-END:variables
 }
